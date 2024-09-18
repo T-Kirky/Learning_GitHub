c82618a8b7f2b41fd23c2af5cab71a5809ea05cf
Paul Di Pasquale <paul.dipasquale.1994@gmail.com>
2024-09-18 10:51:17 +1000

    i added a cos wave in this branch

--- damnit.py
+++ damnit.py
@@ -3,13 +3,27 @@ import matplotlib.pyplot as plt
 
 # Generate data for the sine wave
 x = np.linspace(0, 2 * np.pi, 1000)  # 1000 points between 0 and 2π
-y = np.sin(x)
+y_sin = np.sin(x)
 
 # Create the plot
-plt.plot(x, y)
+plt.plot(x, y_sin)
 
 # Add labels and title
-plt.title('Sine Wave')
+plt.title('Sine Wave - beeosahdkgasdf')
+plt.xlabel('x (radians)')
+plt.ylabel('sin(x)')
+
+# Show the plot
+plt.grid(True)  # Adds gridlines
+plt.show()
+
+#################
+# Now I want to plot a cos function:
+y_cos = np.cos(x)
+
+plt.plot(x, y_cos)
+
+plt.title('Cose wave')
 plt.xlabel('x (radians)')
 plt.ylabel('sin(x)')
 
